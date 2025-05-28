f57f5684ae5ddba964f9452b48919c7b536ad5a0
Shwetank Thakur <shwetankthakur@outlook.com>
2025-05-28 11:41:07 +0530

    feat(models): add User and Document models with DbContext configuration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DocumentStorageSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentStorageSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Document> Documents { get; set; }
+    }
+}
+
---
+++ Models/Document.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentStorageSystem.Models
+{
+    public class Document
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte[] Content { get; set; }
+
+        public int Version { get; set; }
+
+        public DateTime UploadDate { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
